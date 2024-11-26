c5f294e6ebb79177ea82074789283d32a7e6b41d
Matt Heeter <matthewheeter1611@gmail.com>
2024-11-26 10:29:04 -0500

    Added tons of comments and TODOs

--- webserver/helpers.py
+++ webserver/helpers.py
@@ -21,7 +21,7 @@ class Group:
         return len(self.bulletin)
 
     def max_idx(self):
-        return len(self) - 1
+        return max(len(self) - 1, 0)
 
 
 class ThreadedTCPServer(ThreadingMixIn, TCPServer):
@@ -30,108 +30,131 @@ class ThreadedTCPServer(ThreadingMixIn, TCPServer):
 
 class ThreadedTCPRequestHandler(BaseRequestHandler):
     """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
     """
-    groups = {"public": Group("public")}
-
-    groups["public"].add_message(("Message 0 Subject", "Message 0 Body"))
-    groups["public"].add_message(("Message 1 Subject", "Message 1 Body"))
-    groups["public"].add_message(("Message 2 Subject", "Message 2 Body"))
-
-    group_lock = threading.Lock()
-    server_users = set()
+    groups = {"public": Group("public")}  # Defining all groups up-front
+    group_lock = threading.Lock()  # Need to use locks to make any writes to groups or users
+    server_users = set()  # Set of users currently on the server
     user_lock = threading.Lock()
+
+    # TODO: Remove these, they are used for testing
+    # groups["public"].add_message(("Message 0 Subject", "Message 0 Body"))
+    # groups["public"].add_message(("Message 1 Subject", "Message 1 Body"))
+    # groups["public"].add_message(("Message 2 Subject", "Message 2 Body"))
 
     def __init__(self, request, client_address, server):
         super().__init__(request, client_address, server)
-        self.username = None
-        self.message_cutoff = None
+        self.username = None  # The username for the connected client
+        self.message_cutoff = None  # Used to keep track of the "last two message" aspect
 
     def handle(self):
         while True:
+            # Loop to have every user choose a unique username
             self.request.sendall("Enter username:".encode())
             username = self.request.recv(1024).strip().decode()
-            if username not in self.server_users:
-                self.request.sendall(f"Welcome to the server, {username}".encode())
-                break
-            else:
-                self.request.sendall(f"{username} is already in the server, please choose another username.".encode())
+            with self.user_lock:
+                if username not in self.server_users:
+                    self.server_users.add(username)
+                    self.request.sendall(f"Welcome to the server, {username}".encode())
+                    break
+                else:
+                    self.request.sendall(f"{username} is already in the server, please choose another username.".encode())
 
+        # Ensures that a user may not access any message in a group that they are not a part of
+        # TODO: extend to all groups
         self.message_cutoff = {"public": -2}
         self.username = username
 
-        with self.user_lock:
-            self.server_users.add(username)
-            print(self.server_users)
-
         try:
             while True:
+                # Loop to accept and serve commands from clients
                 command = self.request.recv(1024).decode()
                 if command.strip() == "exit":
-                    self.server_users.remove(self.username)
+                    # Command to exit the server
+                    with self.user_lock:
+                        self.server_users.remove(self.username)
                     raise KeyboardInterrupt
 
+                # Splitting the command into the named command and the arguments passed to it
                 method = command.split(" ")[0]
                 args = command.split(" ")[1:]
                 try:
+                    # Try to use the command with the arguments provided
                     getattr(self, method)(*args)
                 except AttributeError:
+                    # If the provided command does not exist
                     self.request.sendall("Invalid command!".encode())
                 except TypeError:
+                    # If the arguments to the command are invalid
                     self.request.sendall(f"Invalid arguments for {method}!".encode())
                 except Exception:
+                    # If some other error occurred (typically inside the command's function)
+                    # TODO: Provide a more useful error message to help the client out?
                     self.request.sendall("An error has occurred, please try again.".encode())
 
         except Exception as e:
+            # For the client leaving the server for any other reason
             self.server_users.remove(username)
             pass
 
     def join(self, group_name):
+        # Joining a group
+        # TODO: Extend to multiple groups
         with self.group_lock:
+            # Adding the username to the group
             self.groups["public"].add_user(self.username)
-            self.message_cutoff["public"] = self.groups["public"].max_idx() - 1
+        # Allowing the user to access the last and second to last most recently added bulletin messages
+        self.message_cutoff["public"] = self.groups["public"].max_idx() - 1
         self.request.sendall(f"You have joined the {group_name} group!".encode())
 
     def message(self, message_idx):
+        # Displaying messages
+        # TODO: Extend to multiple groups
         message_idx = int(message_idx)
-        if self.message_cutoff["public"] < 0:
+        if self.message_cutoff["public"] < -1:
+            # The user is not a part of the group
             self.request.sendall(f"Cannot access messages from group 'public'. Consider joining the group?".encode())
             return
         if not message_idx >= self.message_cutoff["public"]:
+            # The user is trying to access a message that was added more than 2 posts ago
             self.request.sendall(f"Sorry, message {message_idx} cannot be accessed.".encode())
             return
         if message_idx >= len(self.groups["public"].bulletin):
+            # The user is trying to access messages with indices that do not yet exist
             self.request.sendall(f"Message {message_idx} does not exist.".encode())
             return
-        with self.group_lock:
-            subject, body = self.groups['public'][message_idx]
+        subject, body = self.groups['public'][message_idx]
         self.request.send(f"{subject}\n{body}".encode())
 
     def post(self):
+        # Posting to a bulletin
+        # TODO: Extend to multiple groups
         if self.username not in self.groups["public"].users:
+            # The user is not in the group
             self.request.sendall("Cannot post to group 'public'. Consider joining the group?".encode())
             return
+        # Providing separate prompts for subject and body
         self.request.sendall("Enter message subject:".encode())
         message_subject = self.request.recv(1024).decode()
         self.request.sendall("Enter message body:".encode())
         message_body = self.request.recv(1024).decode()
         with self.group_lock:
+            # Adding the message
             self.groups["public"].add_message((message_subject, message_body))
         self.request.sendall("Message posted!".encode())
 
     def users(self):
-        with self.user_lock:
-            users = ("\n".join(self.groups["public"].users))
+        # Displaying the users within a group
+        # TODO: Extend to multiple groups
+        users = ("\n".join(self.groups["public"].users))
         if users:
             self.request.sendall(users.encode())
             return
+        # Case for there being no users
         self.request.sendall("No users in public.".encode())
 
     def leave(self):
+        # Leaving a group
+        # TODO: Extend to multiple groups
         with self.group_lock:
             self.groups["public"].users.remove(self.username)
         self.message_cutoff["public"] = -2
